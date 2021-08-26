860bc304958eb35cf99e530c94605a32fa744304
IuliaCovalenco <61017754+IuliaCovalenco@users.noreply.github.com>
2021-08-26 21:06:36 +0100

    tail

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: [false],
+  purge: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
