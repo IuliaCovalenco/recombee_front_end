d28e9b40f9cd0deef151bedfc3de3d1dcacc7195
IuliaCovalenco <61017754+IuliaCovalenco@users.noreply.github.com>
2021-08-26 22:06:09 +0100

    new

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create ( {
-    baseUrl = "https://pressclubnode.herokuapp.com/api/"
+    baseURL = "https://pressclubnode.herokuapp.com/api/"
 })
